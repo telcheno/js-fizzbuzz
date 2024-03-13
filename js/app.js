51295758bffac889bcfbf4e9b20c0e399afdc94f
telmo-checa <telmocheca1@gmail.com>
2024-03-13 21:15:34 +0100

    Esecuzione del ciclo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("Ciclo For");
+
+//Il CICLO FOR serve a fare una determinata vatutazione mentre la condizione è vera "true", quando la condizione deventa falsa "false" finisce la valutazione e il cilo finisce.
+
+//Inizializare variabile; contorlo la condizione; incremento
+for (let i = 0; i < 10; i++){
+  //Stampa del ciclo mentre la condizione è vera, quando la condizione è falsa il ciclo finisce
+  console.log(i)
+}
